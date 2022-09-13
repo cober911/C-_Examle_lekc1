bcc255510ee482ae349757ca628dcfeb732554de
VladimirE <wortex2014@bk.ru>
2022-09-13 22:54:41 +0400

    .ToLower()

--- Exampe05_ifElse/Program.cs
+++ Exampe05_ifElse/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это же Маша");
+}
+else
+{
+    Console.WriteLine("Привет " + userName);
+}
